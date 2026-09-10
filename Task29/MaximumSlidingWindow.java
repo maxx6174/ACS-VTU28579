6d0149696a1ff316380965f001819a7a30991212
maxx6174 <mrstranger1729@gmail.com>
2026-09-10 09:17:00 +0530

    Implement Maximum Sliding Window algorithm

---
+++ Task29/MaximumSlidingWindow.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class MaximumSlidingWindow {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        int ri = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            dq.offerLast(i);
+
+            if (i >= k - 1) {
+                result[ri++] = nums[dq.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+
+        System.out.println(Arrays.toString(maxSlidingWindow(nums, k)));
+    }
+}
