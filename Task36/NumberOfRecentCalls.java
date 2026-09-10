b7d01203ccddac9b18799beeb4c5a7bda09c4685
maxx6174 <mrstranger1729@gmail.com>
2026-09-10 09:28:29 +0530

    Implement NumberOfRecentCalls class with ping method

---
+++ Task36/NumberOfRecentCalls.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class NumberOfRecentCalls {
+    Queue<Integer> q;
+
+    public NumberOfRecentCalls() {
+        q = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        q.offer(t);
+
+        while (q.peek() < t - 3000) {
+            q.poll();
+        }
+
+        return q.size();
+    }
+}
